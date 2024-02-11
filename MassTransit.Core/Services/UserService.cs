d121efb1e158f7970611105058ade1aff8489713
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2024-02-11 22:36:35 +0500

    INFRA: Initial Create

---
+++ MassTransit.Core/Services/UserService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit.Services.Interfaces;
+
+namespace MassTransit.Services
+{
+    public class UserService : IUserService
+    {
+        
+    }
+}
